ac6a9ceed58b22da240ce7ef01ad0fff238e5a6a
Magnus Skog <magnus.m.skog@gmail.com>
2015-11-17 19:56:03 +0100

    Added case when there is no main story length reported and refactored a bit

--- lib/lita/handlers/howlongtobeat.rb
+++ lib/lita/handlers/howlongtobeat.rb
@@ -1,20 +1,52 @@
 module Lita
   module Handlers
     class Howlongtobeat < Handler
-        SITE_URL = "http://howlongtobeat.com"
-        SEARCH_URL = "http://howlongtobeat.com/search_main.php?page=1"
 
         route(/^(?:hltp|howlongtobeat)\s+(.*)/i, :howlongtobeat, command: true, help: { t("help.howlongtobeat_key") => t("help.howlongtobeat_value")})
 
         def howlongtobeat(response)
-          term = response.matches[0][0]
-          search = http.post(SEARCH_URL, queryString: term, t: 'games', sorthead: 'popular', sortd: 'Normal Order', detail: '0')
-          document = Nokogiri::HTML(search.body)
-          title = document.css('a')[0]
-          response.reply("No results found for #{term}") && return unless title
-          story = document.css(".search_list_tidbit.center")[0].text
-          response.reply("It will take about #{story}to beat the main story of #{title.attr('title')}")
-          response.reply("Source: #{SITE_URL}/#{title.attr('href')}")
+          HowlongtobeatSearch.new(response.matches[0][0], http).print_result(response)
+        end
+
+        class HowlongtobeatSearch
+          SEARCH_URL = "http://howlongtobeat.com/search_main.php?page=1"
+          SITE_URL = "http://howlongtobeat.com"
+
+          def initialize(term, client = Faraday)
+            @term = term
+            @client = client
+          end
+
+          def print_result(response)
+            response.reply("No results found for #{@term}") && return unless title
+            response.reply("No data found for how long it will take to complete the main story of '#{title_content}'") && return unless story
+            response.reply("It will take about #{story}to beat the main story of '#{title_content}'")
+            response.reply("Source: #{SITE_URL}/#{title_href}")
+          end
+
+          def story
+            text = doc.css(".search_list_tidbit.center")[0].text
+            text == '--' ? nil : text
+          end
+
+          def title
+            doc.css('a')[0]
+          end
+
+          def title_href
+            title ? title.attr('href') : nil
+          end
+
+          def title_content
+            title ? title.attr('title') : nil
+          end
+
+          def doc
+            @data ||= begin
+              search = @client.post(SEARCH_URL, queryString: @term, t: 'games', sorthead: 'popular', sortd: 'Normal Order', detail: '0')
+              Nokogiri::HTML(search.body)
+            end
+          end
         end
 
         Lita.register_handler(self)
--- spec/lita/handlers/howlongtobeat_spec.rb
+++ spec/lita/handlers/howlongtobeat_spec.rb
@@ -6,19 +6,31 @@ describe Lita::Handlers::Howlongtobeat, lita_handler: true do
 
   describe "#howlongtobeat" do
     before :each do
-      stub_request(:post, described_class::SEARCH_URL).with(body: {"detail"=>"0", "queryString"=>"sdfdfsfdfds", "sortd"=>"Normal Order", "sorthead"=>"popular", "t"=>"games"}).to_return(File.read('spec/fixtures/nothing_found.txt'))
+      stub_request(:post, described_class::HowlongtobeatSearch::SEARCH_URL).with(body: {"detail"=>"0", "queryString"=>"sdfdfsfdfds", "sortd"=>"Normal Order", "sorthead"=>"popular", "t"=>"games"}).to_return(File.read('spec/fixtures/nothing_found.txt'))
     end
 
     context "with a term with results" do
       before :each do
-        stub_request(:post, described_class::SEARCH_URL).with(body: {"detail"=>"0", "queryString"=>"witcher 3", "sortd"=>"Normal Order", "sorthead"=>"popular", "t"=>"games"}).to_return(File.read('spec/fixtures/witcher_3.txt'))
+        stub_request(:post, described_class::HowlongtobeatSearch::SEARCH_URL).with(body: {"detail"=>"0", "queryString"=>"witcher 3", "sortd"=>"Normal Order", "sorthead"=>"popular", "t"=>"games"}).to_return(File.read('spec/fixtures/witcher_3.txt'))
       end
 
       it "returns the title and the main story length" do
         send_command "howlongtobeat witcher 3"
         expect(replies.count).to eq 2
-        expect(replies[0]).to match 'It will take about 43½ Hours to beat the main story of The Witcher 3: Wild Hunt'
-        expect(replies[1]).to match "Source: #{described_class::SITE_URL}/game.php?id=10270"
+        expect(replies[0]).to match "It will take about 43½ Hours to beat the main story of 'The Witcher 3: Wild Hunt'"
+        expect(replies[1]).to match "Source: #{described_class::HowlongtobeatSearch::SITE_URL}/game.php?id=10270"
+      end
+    end
+
+    context "with a term with no main story results" do
+      before :each do
+        stub_request(:post, described_class::HowlongtobeatSearch::SEARCH_URL).with(body: {"detail"=>"0", "queryString"=>"legacy of the void", "sortd"=>"Normal Order", "sorthead"=>"popular", "t"=>"games"}).to_return(File.read('spec/fixtures/no_main.txt'))
+      end
+
+      it "returns a helpful note" do
+        send_command "howlongtobeat legacy of the void"
+        expect(replies.count).to eq 1
+        expect(replies[0]).to match "No data found for how long it will take to complete the main story of 'StarCraft II: Legacy of the Void'"
       end
     end
 
@@ -26,7 +38,7 @@ describe Lita::Handlers::Howlongtobeat, lita_handler: true do
       let(:command){'sdfdfsfdfds'}
 
       before :each do
-        stub_request(:post, described_class::SEARCH_URL).with(body: {"detail"=>"0", "queryString"=> command, "sortd"=>"Normal Order", "sorthead"=>"popular", "t"=>"games"}).to_return(File.read('spec/fixtures/nothing_found.txt'))
+        stub_request(:post, described_class::HowlongtobeatSearch::SEARCH_URL).with(body: {"detail"=>"0", "queryString"=> command, "sortd"=>"Normal Order", "sorthead"=>"popular", "t"=>"games"}).to_return(File.read('spec/fixtures/nothing_found.txt'))
       end
 
       it "returns an error" do
